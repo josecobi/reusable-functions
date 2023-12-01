34889b8860d94b75993bfc7ce3dc5da5e72b49b6
josecobi <58313777+josecobi@users.noreply.github.com>
2023-12-01 08:40:58 -0500

    added function that returns the average of the  numbers of an array

--- reusable-functions.js
+++ reusable-functions.js
@@ -1,18 +1,23 @@
 //Part 1:  Thinking Functionally
 console.log(`>>>>>>>>>>>>>>>>>>>>>>Part 1:  Thinking Functionally<<<<<<<<<<<<<<<<<<<`);
+// Declare the array of numbers that will be used to test the functions below
+let arrOfNum = [5, 10, 15, 20];
+
 //Take an array of numbers and return the sum.
-
-
 function sumArray(numArr){
     let sum = 0;
     numArr.forEach((number) =>{
-    console.log(number);
     sum += number});
     return sum;
 }
 
-let arrOfNum = [5, 10, 15, 20];
-console.log(arrOfNum[0] + arrOfNum[1]);
-
-
 console.log(`\nThe sum of the numbers in the array [${arrOfNum}] is: ${sumArray(arrOfNum)}`);
+
+//Take an array of numbers and return the average.
+function averageArray(numArr){
+    let sum = sumArray(numArr);
+    let average = sum / numArr.length;
+    return average;
+}
+
+console.log(`\nThe sum of the numbers in the array [${arrOfNum}] is: ${averageArray(arrOfNum)}`);
